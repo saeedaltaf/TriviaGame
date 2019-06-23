0a0b0cbe53a474803691551ddac644dd7e57a020
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-22 21:41:16 -0400

    Updated background image, positions and button styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 //Create start screen with Welcome and Start button
 function startScreen(){
-    startScreen = "<h1 style = 'text-align: center;'>Welcome to the NBA Quiz!</h1><br><br><button type='button' class='btn btn-primary btn-lg btn-block'>TEST</button>";
+    startScreen = "<h1 style = 'text-align: center;'>Welcome to the NBA Quiz!</h1><br><br><button type='button' class='btn btn-primary btn-lg btn-block'>START GAME</button>";
     $(".titleAndStart").html(startScreen);
 
 }
