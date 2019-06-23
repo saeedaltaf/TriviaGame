4c2bbc8e29d6708db1890a659b0d2a6d484dbbd8
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-22 20:43:44 -0400

    testing positioning of start screen

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+//Create start screen with Welcome and Start button
+function startScreen(){
+    startScreen = "<h1 style = 'text-align: center;'>Welcome to the NBA Quiz!</h1><br><br><button type='button' class='btn btn-primary btn-lg btn-block'>TEST</button>";
+    $(".titleAndStart").html(startScreen);
+
+}
+
+startScreen();
+
+})
+//Create functions for timers, wins, losses, reset
+//Create Object with questions and answer choices
+
+
+
+
+
